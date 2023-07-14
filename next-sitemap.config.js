493b4b74e1a2696f3cab2ccee219c81e515afc96
1mamute <gabrielbassinello@hotmail.com>
2023-07-14 15:22:31 +0000

    fix: unoptimized images since we are exporting static and fix the url in the sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://openaudio.com', // FIXME: Change to the production URL
+  siteUrl: 'https://openaudio.co', // FIXME: Change to the production URL
   generateRobotsTxt: true,
 };
